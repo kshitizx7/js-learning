70691c1aaa9d6be0a08b79864ca3e549f0344ded
kshitiz sharma <148480517+kshitizx7@users.noreply.github.com>
2025-04-06 19:56:22 +0000

    inicaial commit of objects

---
+++ 02_basics/objects.js
@@ -0,0 +1,16 @@
+//declare objects
+//singleton
+// when we declare through literals then the object is not singleton
+//object literals
+
+const jsUser = {
+    name : "kshitiz",
+    age : 20,
+    city : "Greater Noida",
+    isLoggedIn : true
+}
+
+console.log(jsUser.age);
+console.log(jsUser.city);
+console.log(jsUser.isLoggedIn);
+console.log(jsUser.name);
