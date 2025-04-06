ad4e84644cb388879eec85152846c52985e63516
kshitiz sharma <148480517+kshitizx7@users.noreply.github.com>
2025-04-06 21:52:22 +0000

    arrays in basics 2

---
+++ 02_basics/array.js
@@ -0,0 +1,31 @@
+// let marvel_heros = ["thor","ironman","spiderman"]
+// let dc_heros = ["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+// let allHeros = marvel_heros.concat(dc_heros);
+// console.log(allHeros);
+
+// let newHeros = [...marvel_heros,...dc_heros] 
+// console.log(newHeros);
+
+// const multiDimArr = [1,2,3,4,5,[[6,7,8],[9,10],11,22,33,44,55,66,77,88,99,100]]
+// const flatArr = multiDimArr.flat(Infinity)
+// console.log(flatArr);   
+
+// let createArr = Array.from("Kshitiz Sharma")   
+// console.log(createArr); // [ 'K', 's', 'h', 'i', 't', 'i', 'z', ' ', 'S', 'h', 'a', 'r', 'm', 'a' ] 
+
+// let checkArr = Array.isArray("kshitiz")
+// console.log(checkArr);
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+// let buildArray = Array.of(score2,score1,score3)
+// console.log(buildArray);
+
+// let buildArray2 = Array.of("Kshitiz");
+// console.log(buildArray2);
+
