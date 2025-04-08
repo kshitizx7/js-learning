26f756a14099e02266695f5873d5ade4afa264f8
kshitiz sharma <148480517+kshitizx7@users.noreply.github.com>
2025-04-08 22:19:05 +0000

    function inside object

--- 03_basics/this_and_Arrow_func.js
+++ 03_basics/this_and_Arrow_func.js
@@ -1,11 +1,16 @@
 const user = {
     username : "Kshitiz",
-    price : "9999"
+    price : "9999",
 
-    const welcomeMessage = function() {
-        console.log(this.username);
-        console.log(this.price);
-        
+    // we can declare function inside object 
+    // but we should assign the name of function 
+    // as we assign the name of variables inside the object
+
+    welcomeMessage : function() {
+        console.log(`hello ${this.username} and price is ${this.price}`);  
     }
+
 }
+
+user.welcomeMessage();
 
