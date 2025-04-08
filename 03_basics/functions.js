5d92bfe9fdbc8afb4d84b2b2aa704b5691d2b644
kshitiz sharma <148480517+kshitizx7@users.noreply.github.com>
2025-04-08 12:28:41 +0000

    functions with object and array

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -1,7 +1,40 @@
-function cartSum(num,existingSum) {
-    return cartSum + existingSum;
+// ... is a rest operator to send multiple values into one single array
+
+
+// function cartSum(...num) {
+//     let sum = 0;
+//     for (let i = 0; i < num.length; i++) {
+//         sum += num[i]
+//     }
+//     return sum;
+// }
+
+// let paisa = cartSum(100,200,300,400,500);
+
+// console.log(paisa);
+
+ 
+//Object in function
+
+function handleObject(obj) {
+    console.log(`Username ${obj.UserName} and price is ${obj.price}`)
 }
 
-existingSum = 500
-console.log(cartSum(1200));
- 
+const obj =  {
+    UserName : "Kshitiz Sharma",
+    price : 1700
+}
+
+// handleObject(obj);
+// handleObject({
+//     UserName : "Ansh",
+//     price : "120000"
+// })
+
+function handelArray(myArr) {
+    for(let i=0; i<myArr.length; i++) {
+        console.log(myArr[i]);
+    }   
+}
+myNewArray = [100,200,300,400,500];
+handelArray(myNewArray)
