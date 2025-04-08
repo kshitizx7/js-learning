de77e3c060937319b7b74add3370f494ef9bdbde
kshitiz sharma <148480517+kshitizx7@users.noreply.github.com>
2025-04-08 14:29:55 +0000

    scope in js

---
+++ 03_basics/scope.js
@@ -0,0 +1,9 @@
+if(true) {
+    userName = "Kshitiz"
+    age = 21
+    if(userName === "Kshitiz") {
+        let prof = "Soldier"
+        console.log(`userName :  ${userName} , age : ${age}`);
+    }
+    console.log(prof);   
+}
