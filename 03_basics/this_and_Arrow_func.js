538966ecfb124029201f4aa7738b6e965db76e5b
kshitiz sharma <148480517+kshitizx7@users.noreply.github.com>
2025-04-08 14:29:22 +0000

    initial commit

---
+++ 03_basics/this_and_Arrow_func.js
@@ -0,0 +1,11 @@
+const user = {
+    username : "Kshitiz",
+    price : "9999"
+
+    const welcomeMessage = function() {
+        console.log(this.username);
+        console.log(this.price);
+        
+    }
+}
+
